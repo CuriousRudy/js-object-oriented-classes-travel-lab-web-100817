c12912791bc3a7bb25a5c453a278c353b4456097
Abe Rudy <aberudy@gmail.com>
2017-11-21 14:24:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+  constructor(name, dateString) {
+    this.name = name
+    this.startDate = new Date(dateString)
+  }
+  yearsExperienceFromBeginningOf (arg) {
+    let start = this.startDate.getFullYear()
+    return (arg - 1) - start
+  }
+}
 
+let horizontals =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor (startObj, endObj) {
+    this.startHorizontal = startObj['horizontal']
+    this.startVertical = startObj['vertical']
+    this.endHorizontal = endObj['horizontal']
+    this.endVertical = endObj['vertical']
+  }
+  blocksTravelled () {
+    let eastWest = Math.abs(horizontals.indexOf(this.startHorizontal) - horizontals.indexOf(this.endHorizontal))
+    let northSouth = Math.abs(this.startVertical - this.endVertical)
+    return eastWest + northSouth
+  }
+  estimatedTime (trip) {
+    let blocks = this.blocksTravelled()
+    if (trip) {
+      return blocks / 2
+    } else {
+      return blocks / 3
+    }
+  }
+}
